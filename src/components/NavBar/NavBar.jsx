c6012115e90db0f67a1c822c3d9285a9dc8c362f
Bapan Modak <bapanm2317@gmail.com>
2026-02-13 20:28:34 +0530

    done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
+import "./navbar.css";
 import React, { useState } from "react";
-
 import { Link, NavLink } from "react-router";
 import logo from "../../assets/logo.png";
 import { IoMdClose, IoMdMenu } from "react-icons/io";
 import { FaAnglesRight } from "react-icons/fa6";
-import "./navbar.css";
+
 import AnimatedButton from "../sheared/AnimatedButton";
 import { map } from "motion/react-client";
 
