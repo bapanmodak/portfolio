e3e0a354287ca7b3956434f7bcc47f95f44ff8e9
Bapan Modak <bapanm2317@gmail.com>
2026-02-13 20:17:42 +0530

    some problem fixed

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,34 +4,34 @@ import MainLayout from "../Layouts/MainLayout";
 import HomePage from "../pages/HomePage/HomePage";
 import AboutPage from "../pages/AboutPage";
 import ProjectsPage from "../pages/ProjectsPage";
-import ContactUsPage from "../pages/contactUsPage";
+import ContactUsPage from "../pages/ContactUsPage";
 import SkillsPage from "../pages/SkillsPage";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayout />,
-        children: [
-            {
-                index: true,
-                element: <HomePage />
-            },
-            {
-                path: '/about',
-                element: <AboutPage />
-            },
-            {
-                path: '/projects',
-                element: <ProjectsPage />
-            },
-            {
-                path: '/contact',
-                element: <ContactUsPage />
-            },
-            {
-                path: '/skills',
-                element: <SkillsPage />
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUsPage />,
+      },
+      {
+        path: "/skills",
+        element: <SkillsPage />,
+      },
+    ],
+  },
+]);
