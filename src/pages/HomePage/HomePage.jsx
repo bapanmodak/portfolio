63a22216221b31c8e3020646ae9e76535ae4e963
Bapan Modak <bapanm2317@gmail.com>
2026-02-16 19:14:04 +0530

    seo testing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import Hero from '../../components/Hero';
-import About from '../../components/About';
-import Projects from '../../components/Projects';
-import ContactUs from '../../components/ContactUs';
-import Skills from '../../components/Skills';
+import React from "react";
+import Hero from "../../components/Hero";
+import About from "../../components/About";
+import Projects from "../../components/Projects";
+import ContactUs from "../../components/ContactUs";
+import Skills from "../../components/Skills";
+import SEO from "../../components/SEO";
 
 const HomePage = () => {
-    return (
-        <div className=''>
-            <Hero />
-            <div className="">
-                <About />
-                <Projects />
-                <ContactUs />
-                <Skills />
-            </div>
-        </div>
-    );
+  return (
+    <div className="">
+      {/* Dynamic SEO tags for the Home page */}
+      <SEO
+        title="Home"
+        description="Welcome to my portfolio website! I'm Bapan Modak, a passionate web developer specializing in creating stunning and functional websites. Explore my projects, skills, and contact me for collaborations."
+      />
+      <Hero />
+      <div className="">
+        <About />
+        <Projects />
+        <ContactUs />
+        <Skills />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
